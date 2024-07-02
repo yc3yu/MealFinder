cff89b40761b3bb49c696574f13d6f0ed4d2d326
cindyu1020 <cindyu1020@gmail.com>
2024-07-02 16:22:43 -0700

    Initial code commit

---
+++ MealFinder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MealFinder
+//
+//  Created by Yue chen Yu on 2024-07-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MealFinder/MealFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealFinderApp.swift
+//  MealFinder
+//
+//  Created by Yue chen Yu on 2024-07-02.
+//
+
+import SwiftUI
+
+@main
+struct MealFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
